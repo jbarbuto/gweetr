e8b86702661cbf6af4b770acae5777e95689bb09
John A. Barbuto <jbarbuto@users.noreply.github.com>
2014-01-10 11:33:26 -0800

    Added classifier indicating Python 2 support

--- setup.py
+++ setup.py
@@ -28,6 +28,7 @@ setup(
         'Framework :: Flask',
         'License :: OSI Approved :: MIT License',
         'Operating System :: POSIX',
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Topic :: Communications :: Internet Phone',
