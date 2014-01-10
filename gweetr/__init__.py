e499ec8a682ff05c16a478f136ca14b9ca5469ae
John A. Barbuto <jbarbuto@users.noreply.github.com>
2014-01-10 11:28:23 -0800

    Properly support proxy setups

--- gweetr/__init__.py
+++ gweetr/__init__.py
@@ -2,11 +2,14 @@
 
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from werkzeug.contrib.fixers import ProxyFix
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_pyfile('settings.cfg')
 app.config.from_envvar('GWEETR_CONFIG_PATH', silent=True)
 app.secret_key = app.config['APP_SECRET_KEY']
+if app.config['USE_PROXY']:
+    app.wsgi_app = ProxyFix(app.wsgi_app)
 
 db = SQLAlchemy(app)
 
